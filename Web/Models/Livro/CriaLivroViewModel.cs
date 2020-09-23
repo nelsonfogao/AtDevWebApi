15acfde89eb8b92ec24d922fcb73969a0b3b7e8d
Nelson Barros <nelsonfogao@users.noreply.github.com>
2020-09-23 20:33:27 -0300

    livroobrigatorio

--- Web/Models/Livro/CriaLivroViewModel.cs
+++ Web/Models/Livro/CriaLivroViewModel.cs
@@ -9,15 +9,15 @@ namespace Web.Models.Livro
 {
     public class CriaLivroViewModel
     {
+
+        [Required(ErrorMessage = "Campo Título é obrigatório")]
+
         public string Titulo { get; set; }
-
-
+        [Required(ErrorMessage = "Campo ISBN é obrigatório")]
         public string ISBN { get; set; }
-
-
+        [Required(ErrorMessage = "Campo Ano é obrigatório")]
         public int Ano { get; set; }
-
-
+        [Required(ErrorMessage = "Campo Autor é obrigatório")]
         public List<AutorViewModel> Autors { get; set; }
 
         public int AutorId { get; set; }
